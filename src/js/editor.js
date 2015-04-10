7784f45f1ad87964cf115dc192a2055873d6e94f
Dustin Smith <thedustinsmith@gmail.com>
2015-04-09 22:56:25 -0500

    tooltip on links

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,6 +1,41 @@
 (function (app) {
 
 	var Editor = {
+		initLinkHelper: function (editor) {
+			var linkHelpTimer;
+			var tipElement;
+			var tip;
+			editor.on('interaction:composer', function (e) {
+				var range = editor.composer.selection.getRange();
+				var container = range.commonAncestorContainer;
+				if (container.nodeType == 3) {
+					container = container.parentNode;
+				}
+				if (container.nodeName === "A") {
+					if (tipElement == container) {
+						return;
+					}
+					tipElement = container;
+					linkHelpTimer = setTimeout(function () {
+						tip = new Opentip(tipElement, 'this is a link', { 
+							target: true,
+							showOn: null,
+							removeElementsOnHide: true,
+							hideOn: 'none'
+						});
+						tip.show();
+					}, 1000);
+				}
+				else {
+					clearTimeout(linkHelpTimer);
+					tipElement = null;
+					if (tip) {
+						tip.deactivate();
+						tip = null;
+					}
+				}
+			});
+		},
 		initClipboard: function () {
 			var btn = document.getElementById('copy-btn');
 			var copyBtn = new ZeroClipboard(btn);
@@ -26,6 +61,7 @@
 			});
 			editor.initFileUpload();
 			this.initClipboard();
+			this.initLinkHelper(editor);
 		}
 	};
 
