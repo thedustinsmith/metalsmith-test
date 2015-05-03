95fa1e346e7903dbf97dee99da0f0362b6911704
Dustin Smith <thedustinsmith@gmail.com>
2015-05-03 18:16:17 -0500

    Some editor Changes

--- build.js
+++ build.js
@@ -7,6 +7,7 @@ var Metalsmith  	= require('metalsmith'),
     watch 			= require('metalsmith-watch'),
     serve			= require('metalsmith-serve')
     assets 			= require('metalsmith-assets'),
+    concat			= require('metalsmith-concat'),
     helpers 		= require('./tools/helpers'),
     cleanUrls 		= helpers.cleanUrls,
     fixResources 	= helpers.fixRelativeResources,
@@ -31,13 +32,39 @@ var swigInPlace = {
 /* End Swig Opts */
 
 
+/* Editor Compiled */
+var editorJs = {
+	files: [
+		'bower_components/zeroclipboard/dist/ZeroClipboard.min.js',
+		'bower_components/mustache/mustache.min.js',
+		'bower_components/opentip/downloads/opentip-native.js',
+		'bower_components/wysihtml/dist/wysihtml5x-toolbar.min.js',
+		'bower_components/wysihtml/parser_rules/advanced_and_extended.js',
+		'js/editor/dialogs.js',
+		'js/editor/filereader.js',
+		'js/editor/editor.fileuploader.js',
+		'js/editor/editor.links.js'
+	],
+	output: 'js/editor.compiled.js'
+};
+var editorCss = {
+	files: [
+		'bower_components/opentip/css/opentip.css',
+		'css/editor.css'
+	],
+	output: 'css/editor.compiled.css'
+};
+/* End Editor Compiled */
+
+
 var ms = Metalsmith(__dirname)
 	.use(cleanUrls())
 	.use(collections({
 		stuff: { refer: false }
 	}))
 	.use(crumbs())
-	.use(each(function (file, name) { // This serves to help swig with template inheritance
+	.use(each(function (file, name) { 
+		// This serves to help swig with template inheritance
 		file.filename = name;
 	}))
 	.use(templates(swigInPlace))
@@ -53,6 +80,8 @@ var ms = Metalsmith(__dirname)
 		source: './bower_components',
 		destination: './bower_components'
 	}))
+	.use(concat(editorJs))
+	.use(concat(editorCss))
 	.use(lunr())
 	.destination('./build');
 
--- src/js/editor.js
+++ src/js/editor.js
@@ -1,40 +1,104 @@
 (function (app) {
 
-	var Editor = {
-		initClipboard: function () {
-			var btn = document.getElementById('copy-btn');
-			var copyBtn = new ZeroClipboard(btn);
-			copyBtn.on('ready', function () {
-				copyBtn.on('copy', function (e) {
-					var clipboard = e.clipboardData;
-					clipboard.setData("text/plain", document.getElementById('editor').innerHTML);
-				});
+	// https://gist.github.com/thedustinsmith/4394c416a316e8046753
+	// Copied from Facebook's logic
+	// I couldn't find a reliable search to keep finding it.
+	// Depends on existing scripts on the page
+	function loadScript (url, callback) {
+	  (function (d, s, id) {
+	    var js, fjs = d.getElementsByTagName(s)[0];
+	    if (d.getElementById(id)) { callback(); }
+	    js = d.createElement(s); js.id = id;
+	    js.onload = function () {
+	        callback();
+	    };
+	    js.src = url; // url
+	    fjs.parentNode.insertBefore(js, fjs);
+	  }(document, 'script', 'dynamic-script'));
+	}
 
-				var toolTip = new Opentip('#copy-btn', 'HTML Copied', {
-					hideDelay: 3,
-					target: true
-				});
-				copyBtn.on('aftercopy', function (e) {
-					toolTip.show();
-				});
-			});
-		},
-		init: function() {
-			var editor = this.editor = new wysihtml5.Editor('editor', {
-				toolbar: 'toolbar',
-				parserRules:  wysihtml5ParserRules
-			});
-			this.initClipboard();
+	var cssResources = [
+		'/css/editor.compiled.css',
+		'/bower_components/fontawesome/css/font-awesome.css'
+	];
+	var jsUrl = "/js/editor.compiled.js";
+	var templateUrl = '/editor-templates/';
 
-			$('#toolbar').on('mousedown', '[data-wysihtml-helper]', function (ev) {
-				ev.preventDefault();
-			});
-			editor.initFileUpload();
-			editor.initLinkHelper();
-		}
+	var Editor = function (contentId) {
+		var self = this; 
+		$('body').addClass('editor');
+		this.contentSelector = contentId;
+		this.contentEl = document.getElementById(contentId);
+		this.$contentEl = $(this.contentEl);
+		this.load(function () {
+			self.$contentEl.prop("contenteditable", true);
+			setTimeout(function(){
+				self.init();
+			}, 1000);
+		});
 	};
 
-	app.Editor = Editor;
+	Editor.prototype.initClipboard = function () {
+		var self = this;
+		var btn = document.getElementById('copy-btn');
+		ZeroClipboard.config({
+			swfPath: '/bower_components/zeroclipboard/dist/ZeroClipboard.swf'
+		});
+		var copyBtn = new ZeroClipboard(btn);
+		copyBtn.on('ready', function () {
+			copyBtn.on('copy', function (e) {
+				var clipboard = e.clipboardData;
+				clipboard.setData("text/plain", self.contentEl.innerHTML);
+			});
 
-})(app);
+			var toolTip = new Opentip('#copy-btn', 'HTML Copied', {
+				hideDelay: 3,
+				target: true
+			});
+			copyBtn.on('aftercopy', function (e) {
+				toolTip.show();
+			});
+		});
+	};
+
+	Editor.prototype.load = function (cb) {
+		var self = this,
+			def = $.Deferred(),
+			templatePromise = def.promise();
+
+		cssResources.forEach(function (u) {
+			$("head").append('<link type="text/css" rel="Stylesheet" href="' + u + '" />');
+		});
+
+		this.templateContainer = $('<div id="editor-templates" />').appendTo("body").hide();
+		this.templateContainer.load(templateUrl, function () {
+			def.resolve();
+		});
+		templatePromise.done(function(){
+			self.toolbar = self.templateContainer.find("#toolbar").hide().insertBefore(self.$contentEl);
+			self.toolbar.fadeIn();
+		});
+		loadScript(jsUrl, function() { 
+			templatePromise.done(function () {
+				cb();
+			});
+		});
+	};
+
+	Editor.prototype.init = function () {
+		var editor = this.editor = new wysihtml5.Editor(this.contentSelector, {
+			toolbar: 'toolbar',
+			parserRules:  wysihtml5ParserRules
+		});
+		this.initClipboard();
+
+		$('#toolbar').on('mousedown', '[data-wysihtml-helper]', function (ev) {
+			ev.preventDefault();
+		});
+		editor.initFileUpload();
+		editor.initLinkHelper();
+	};
+
+	window.MetalEditor = Editor;
+})();
 
